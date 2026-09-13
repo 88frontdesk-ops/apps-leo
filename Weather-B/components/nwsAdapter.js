950b8409f6c7a96dea24b59b12cb432c5615dd3d
88frontdesk-ops <88frontdesk@gmail.com>
2026-09-13 12:18:15 -0400

    Remove obsolete telemetry compatibility shim

--- Weather-B/components/nwsAdapter.js
+++
@@ -1,4 +0,0 @@
-window.mp_subscription=window.mp_subscription||(()=>{});
-window.mp_setting=window.mp_setting||(()=>{});
-window.mp_visits=window.mp_visits||(()=>{});
-window.mp_event=window.mp_event||(()=>{});
