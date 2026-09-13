4f9d90391702b7b3cc3390dad6ae5e1a145d1ca0
88frontdesk-ops <88frontdesk@gmail.com>
2026-09-13 10:28:46 -0400

    Restore Weather-B location defaults

--- Weather-B/background.js
+++ Weather-B/background.js
@@ -1,2 +1,3 @@
-chrome.runtime.onInstalled.addListener(()=>chrome.alarms.create("intervalUpdateTimes",{delayInMinutes:15,periodInMinutes:15}));
-chrome.runtime.onStartup.addListener(()=>chrome.alarms.create("intervalUpdateTimes",{delayInMinutes:15,periodInMinutes:15}));
+const DEFAULTS={citys:"New York",latlong:"40.713,-74.0072",timezone:"America/New_York",country:"US",setSettingFC:"c"};
+chrome.runtime.onInstalled.addListener(()=>chrome.storage.local.set(DEFAULTS));
+chrome.runtime.onStartup.addListener(()=>chrome.storage.local.get(Object.keys(DEFAULTS),d=>{const m={};for(const k in DEFAULTS)if(d[k]===undefined)m[k]=DEFAULTS[k];if(Object.keys(m).length)chrome.storage.local.set(m)}));
